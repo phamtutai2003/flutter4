6c9345c4710e9b34c5c5172197182a75f44d8eb1
phạm tú tài <93572750+phamtutai2003@users.noreply.github.com>
2023-09-03 16:41:19 +0700

    up flutter

---
+++ lib/TutoriaHome.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main (){
+  runApp(
+    const MaterialApp(
+      title: 'Flutter Tutorial',
+      home: TutorialHome(),
+    ),
+  );
+}
+
+class TutorialHome extends StatelessWidget{
+  const TutorialHome ({super.key});
+
+  @override
+  Widget build(BuildContext context){
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: const IconButton(
+            icon:Icon(Icons.menu),
+            tooltip: 'Navigation menu',
+            onPressed: null,
+        ),
+        title: const Text('Example title'),
+        actions: const[
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+
+            onPressed: null,
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text('Hello, world!'),
+      ),
+      floatingActionButton: const FloatingActionButton(
+        tooltip: 'Add',
+          onPressed: null,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
