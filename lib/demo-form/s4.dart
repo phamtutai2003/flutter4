d0c2ef52cd2ceeae43c76c51649cdd6df76133ba
phạm tú tài <93572750+phamtutai2003@users.noreply.github.com>
2023-09-18 21:49:43 +0700

    form

---
+++ lib/demo-form/s4.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ki4/demo-form/s1.dart';
+
+void main() => runApp(const MyApp());
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return const MaterialApp(
+      title: 'Text Field Focus',
+        home: MyCustomForm(),
+    );
+  }
+}
+
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({super.key});
+
+  @override
+  State<MyCustomForm> createState() => _MyCustomFormState();
+}
+
+class _MyCustomFormState extends State<MyCustomForm>{
+  late FocusNode myFocusNode;
+  @override
+  void initState(){
+    super.initState();
+
+    myFocusNode = FocusNode();
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Text Field Focus'),
+      ),
+      body:Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            const TextField(
+              autofocus: true,
+            ),
+            TextField(
+              focusNode: myFocusNode,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => myFocusNode.requestFocus(),
+        tooltip: 'Focus Second Text Field',
+        child: const Icon(Icons.edit),
+      ),
+    );
+  }
+}
