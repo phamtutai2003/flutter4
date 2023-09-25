4cba3ff84c58224e8e9419007e60b4933d99e96a
phạm tú tài <93572750+phamtutai2003@users.noreply.github.com>
2023-09-25 16:31:26 +0700

    ex3

---
+++ lib/c8_senddata/ex3.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  static const appTitle = 'Drawer Demo';
+
+  @override
+  Widget build(BuildContext context){
+    return const MaterialApp(
+      title:appTitle,
+      home: MyHomePage(title: appTitle),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget{
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>{
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+    TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style:optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style:optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style:optionStyle,
+    ),
+
+  ];
+
+  void _onItemTapped(int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar:AppBar(
+        title: Text(widget.title)),
+      body:Center(
+        child: _widgetOptions[_selectedIndex],
+      ),
+      drawer:Drawer(
+        child:ListView(
+
+          padding: EdgeInsets.zero,
+          children:[
+            const DrawerHeader(
+              decoration:BoxDecoration(
+                color:Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Home'),
+              selected: _selectedIndex == 0,
+              onTap:(){
+
+                _onItemTapped(0);
+
+                Navigator.pop(context);
+              },
+            ),
+
+            ListTile(
+              title: const Text('Business'),
+              selected: _selectedIndex == 1,
+              onTap:(){
+
+                _onItemTapped(1);
+
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('School'),
+              selected: _selectedIndex == 2,
+              onTap:(){
+
+                _onItemTapped(2);
+
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
